39833371f1ef197fe4ad45762c1697c84ebb0d27
DavideDaniel <davidedaniel@gmail.com>
2015-01-27 17:03:51 -0500

    hogwarts express started

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require( 'express' );
+var bodyParser = require( 'body-parser' );
+var methodOverride = require('method-override')
+var app = express();
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+app.use(express.static(__dirname + '/public'));
+
+
+var students = {
+	0: {
+		id: 0,
+		name: "David",
+		age: "18",
+		favorite_spell: "apparating"
+	}
+}
+
+var counter = 1
+
+app.get( '/students', function ( req, res ) {
+	res.render( 'index.ejs', {
+		students: students
+	} )
+} );
+
+app.listen( 3000 );
+
+console.log( "Server listening on port: 3000" );
