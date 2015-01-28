8670cb8ce3dcf910e97b3a511e84ed8d51aa88fd
DavideDaniel <davidedaniel@gmail.com>
2015-01-28 03:40:24 -0500

    it all works yay

--- app.js
+++ app.js
@@ -17,16 +17,10 @@ console.log( "that's parsed" );
 
 var students = parsedRoster;
 console.log( students );
-var counter = 1
+var counter = 1;
 
 var none = {
 	no: "No such student is registered."
-};
-
-var houses = ["Gryffindor","Ravenclaw","Hufflepuff","Slytherin"];
-
-var sortingHat = function(student, houses){
-	student.house = houses[Math.floor(Math.random()*houses.length)];
 };
 
 app.get( '/students', function ( req, res ) {
@@ -48,17 +42,19 @@ app.get( '/search', function ( req, res ) {
 
 app.get( '/search/:queryName', function ( req, res ) {
 	var queryName = req.params.queryName
-	console.log("searching for: "+ queryName);
-	for(student in students){
-    			var name = students[student].name;
-    			 	if (queryName === name ){
-    			 		console.log(students[student].name);
-		res.json({exists: students[student]});
+	console.log( "searching for: " + queryName );
+	for ( student in students ) {
+		var name = students[ student ].name;
+		if ( queryName === name ) {
+			console.log( students[ student ].name );
+			res.json( {
+				exists: students[ student ]
+			} );
+		}
+		if ( queryName != name ) {
+			res.json( none )
+		}
 	}
-	if (queryName != name) {
-		res.json(none)
-	}
-}
 } );
 
 app.post( '/student', function ( req, res ) {
@@ -85,13 +81,32 @@ app.put( '/student/:id', function ( req, res ) {
 	req.method = 'get';
 	fs.writeFile( 'roster.json', JSON.stringify( students ) )
 	res.redirect( '/student/' + req.params.id )
-} )
+} );
 
 app.delete( '/student/:id', function ( req, res ) {
 	delete students[ req.params.id ];
 	req.method = 'get';
 	fs.writeFile( 'roster.json', JSON.stringify( students ) )
 	res.redirect( '/students' );
+} );
+
+app.post( '/sort/:id', function ( req, res ) {
+	var houses = [ "Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin" ];
+	for ( student in students ) {
+		if ( students[ student ].id == req.params.id ) {
+			students[ student ].house = houses[ Math.floor( Math.random() * houses.length ) ];
+		}
+	}
+	req.method = 'get';
+	res.redirect( '/students' );
+} );
+
+app.get( '/houses/:house_name', function ( req, res ) {
+	var house = req.params.house_name;
+	res.render( 'house.ejs', {
+		house: house,
+		students: students
+	} );
 } );
 
 app.listen( 3000 );
