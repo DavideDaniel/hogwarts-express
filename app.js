36055efb699b3eb1019aa6673205db39f05d3e6d
DavideDaniel <davidedaniel@gmail.com>
2015-01-27 19:45:05 -0500

    something's broken

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require( 'express' );
 var bodyParser = require( 'body-parser' );
-var methodOverride = require('method-override')
+
+var methodOverride = require( 'method-override' )
 var app = express();
 
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
-app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser());
+app.use( methodOverride( '_method' ) );
+app.use( express.static( __dirname + '/public' ) );
 
 var students = {
 	0: {
@@ -26,12 +26,40 @@ app.get( '/students', function ( req, res ) {
 } );
 
 app.get( '/student/:id', function ( req, res ) {
-	var thisStudent = students[req.params.id]
+	var thisStudent = students[ req.params.id ]
 	res.render( 'show.ejs', {
 		thisStudent: thisStudent
 	} )
 } );
 
+app.post( '/student', function ( req, res ) {
+	var student = {
+		id: counter,
+		name: req.body.name,
+		age: req.body.age,
+		favorite_spell: req.body.faveSpell,
+	};
+	console.log(student);
+	students[counter] = student;
+	counter++
+	console.log(students.counter);
+	res.redirect( '/students' );
+} );
+
+app.put( '/student/:id', function ( req, res ) {
+	console.log( students[ req.params.id ].name );
+	students[ req.params.id ].name = req.body.newName;
+	students[ req.params.id ].age = req.body.newAge;
+	students[ req.params.id ].favorite_spell = req.body.newFaveSpell;
+	req.method = 'get';
+	res.redirect( '/student/' + req.params.id )
+} )
+
+app.delete( '/student/:id', function ( req, res ) {
+	delete students[ req.params.id ];
+	req.method = 'get';
+	res.redirect( '/students' );
+} );
 
 app.listen( 3000 );
 
